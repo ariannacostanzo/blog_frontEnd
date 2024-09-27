64e5d12f5a5d464dce640e198dad352d96872e68
ariannacostanzo <costanzo.arianna@outlook.it>
2024-09-27 13:12:25 +0200

    adding detail page of post

--- src/views/PostShow.vue
+++ src/views/PostShow.vue
@@ -2,16 +2,20 @@
 import axios from 'axios';
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import LoaderApi from '@/components/LoaderApi.vue';
 
 const route = useRoute();
 const post = ref({});
 const baseUrl = 'http://localhost:8000/posts/';
 const slug = route.params.slug;
+const isLoading = ref(false)
 
 const fetchPost = async () => {
+    isLoading.value = true
     try {
         const res = await axios.get(baseUrl + slug);
         post.value = res.data;
+        isLoading.value = false;
     } catch (error) {
         console.log(error)
     }
@@ -25,9 +29,18 @@ fetchPost();
 
 <template>
     <main>
-       <div class="container mx-auto my-5 text-center">
+        <LoaderApi v-if="isLoading"/>
+       <div v-else class="container mx-auto my-5 text-center">
         <h2 class="font-bold text-5xl">{{ post.title }}</h2>
         <p class="mt-5">{{ post.content }}</p>
+        <p v-if="post.categoryId">
+            {{ post.categoryId }}
+        </p>
+        <div v-if="post.tags">
+            <ul>
+                <li v-for="tag in post.tags">{{ tag.name }}</li>
+            </ul>
+        </div>
        </div>
 
        <RouterLink class="ml-5" to="/">
