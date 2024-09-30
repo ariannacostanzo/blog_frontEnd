6a02445c94f1a72f95f4c66474eb388e729ca789
ariannacostanzo <costanzo.arianna@outlook.it>
2024-09-30 19:43:59 +0200

    created searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const searchedTerm = ref('');
+const emit = defineEmits();
+
+
+const filterPosts = () => {
+    emit('search', searchedTerm.value);
+}
+</script>
+
+<template>
+    <div>
+        <input type="text" id="searchbar" placeholder="Zelda..." v-model="searchedTerm" @keydown.enter="filterPosts">
+        <button class="custom-span ml-4" @click="filterPosts">Cerca</button>
+    </div>
+</template>
+
+<style scoped>
+input {
+    padding: 2px 10px;
+    color: var(--custom-black);
+}
+</style>
