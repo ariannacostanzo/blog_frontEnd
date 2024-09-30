841c0a3fbca0be53cdd2374297fda1d8c548174c
ariannacostanzo <costanzo.arianna@outlook.it>
2024-09-30 14:16:31 +0200

    created jumbo component

---
+++ src/components/MainJumbotron.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <figure>
+    <img src="../assets/images/background.jpg" alt="">
+  </figure>
+</template>
+
+<style scoped>
+  figure {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    opacity: .4;
+    z-index: -1;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit:cover;
+    object-position: top center;
+    position: absolute;
+    z-index: -1;
+  }
+</style>
