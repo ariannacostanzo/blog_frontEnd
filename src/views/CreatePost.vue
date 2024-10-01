5dc82a8a3e7e7392ec13211805a9c9af9178ddd5
ariannacostanzo <costanzo.arianna@outlook.it>
2024-10-01 19:53:33 +0200

    adding fron'end validation

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -4,6 +4,7 @@ import LoaderApi from '@/components/LoaderApi.vue';
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+import MainJumbotron from '@/components/MainJumbotron.vue';
 
 //variabili
 const router = useRouter();
@@ -11,48 +12,227 @@ const baseUrl = 'http://localhost:8000/posts';
 const baseData = ref({
     title: '',
     content: '',
+    section: '',
     published: true,
-    tags: []
+    tags: [],
+    categoryId: null
 });
 const post = ref({})
-const isLoading = ref(false)
-
+const isLoading = ref(false);
+//categorie api
+const categories = ref([]);
+const baseUrlCategories = 'http://localhost:8000/categories';
+//errori
+const errors = ref([]);
+const titleErrors = [];
+const contentErrors = [];
+const sectionErrors = [];
+const categoryErrors = [];
+const publishedErrors = [];
+// conteggio dei caratteri
+let contentCharCount = ref(baseData.value.content.length);
+let sectionCharCount = ref(baseData.value.section.length);
+let titleCharCount = ref(baseData.value.title.length);
+const maxContentChar= 100;
+const maxSectionChar= 2000;
+const maxTitleChar = 100;
+const minChar = 3;
 
 //funzioni
 const sendPost = async () => {
+    emptyErrors(titleErrors);
+    emptyErrors(contentErrors);
+    emptyErrors(sectionErrors);
+    emptyErrors(categoryErrors);
+    emptyErrors(publishedErrors);
     isLoading.value = true
     try {
         const res = await axios.post(baseUrl, baseData.value);
         post.value = res.data;
-        console.log(post.value)
         router.push({name: 'detailPost', params: {slug: post.value.slug}})
         isLoading.value = false;
         
+    } catch (error) {
+        
+        errors.value = error.response.data.errors;
+        validation();
+    } finally {
+        isLoading.value = false;
+    }
+}
+
+const emptyErrors = (container) => {
+    container.length = 0
+}
+
+const validation = () => {
+    if (errors.value.length > 0) {
+        errors.value.forEach(error => {
+            if (error.path === "title") {
+                titleErrors.push(error.msg)
+            } 
+            if (error.path === "content") {
+                contentErrors.push(error.msg)
+            } 
+            if (error.path === "section") {
+                sectionErrors.push(error.msg)
+            } 
+            if (error.path === "categoryId") {
+                categoryErrors.push(error.msg)
+            } 
+            if (error.path === "published") {
+                publishedErrors.push(error.msg)
+            } 
+        })
+        return
+    }
+}
+
+const fetchCategories = async () => {
+    isLoading.value = true
+    try {
+        const res = await axios.get(baseUrlCategories);
+        categories.value = res.data;
+        isLoading.value = false;
     } catch (error) {
         console.log(error)
     }
 }
 
+const updateCharacters = (field) => {
+    if (field === 'content') {
+        contentCharCount.value = baseData.value.content.length
+    } else if (field === 'title') {
+        titleCharCount.value = baseData.value.title.length
+    }  else {
+        sectionCharCount.value = baseData.value.section.length
+    }
+}
+
+onMounted(() => {
+    fetchCategories()
+
+})
+
 
 </script>
 
 <template>
-    <main class="container mx-auto my-5 flex items-center justify-center">
+    <main class="container mx-auto my-5">
+        <h1 class="text-center text-4xl mb-5">Crea un nuovo post</h1>
         <LoaderApi v-if="isLoading" />
-        <form @submit.prevent="sendPost" v-else>
-            <div class="my-5 input-container">
-                <label for="title">Titolo post</label>
-                <input id="title" type="text" v-model="baseData.title" required />
+        <form @submit.prevent="sendPost" v-else class="form-container relative">
+            <MainJumbotron />
+
+            <div class="my-5 input-row">
+                <!-- titolo  -->
+                <div class="input-container">
+                    <label for="title">Titolo:</label>
+                    <div>
+                        <div class="relative">
+                            <input @input="updateCharacters('title')" class="pb-4" id="title" type="text"
+                                v-model="baseData.title" />
+                            <p v-if="titleCharCount > 0" class="absolute right-[3px] bottom-[0px]">
+                                <span
+                                    :class="[titleCharCount <= minChar || titleCharCount >= maxTitleChar ? 'text-[#f60001]' : '']">{{
+                                    titleCharCount
+                                    }}</span> / <span
+                                    :class="[titleCharCount <= minChar || titleCharCount >= maxTitleChar ? 'text-[#f60001]' : '']">{{
+                                    maxTitleChar }}</span>
+                            </p>
+                        </div>
+                        <div class="errors-container">
+                            <p v-for="error in titleErrors">{{ error }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <!-- descrizione  -->
+                    <label for="content">Descrizione:</label>
+                    <div>
+                        <div class="relative">
+                            <textarea @input="updateCharacters('content')" rows="4" name="content" id="content"
+                                v-model="baseData.content" class="pb-4 py-1">
+                            </textarea>
+                            <p v-if="contentCharCount > 0" class="absolute right-[8px] bottom-[8px]">
+                                <span
+                                    :class="[contentCharCount <= minChar || contentCharCount >= maxContentChar ? 'text-[#f60001]' : '']">{{
+                                    contentCharCount
+                                    }}</span> /
+                                <span
+                                    :class="[contentCharCount <= minChar || contentCharCount >= maxContentChar ? 'text-[#f60001]' : '']">{{
+                                    maxContentChar }}</span>
+                            </p>
+                        </div>
+                        <div class="errors-container">
+                            <p v-for="error in contentErrors">{{ error }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="my-5 input-container">
-                <label for="content">Contenuto post</label>
-                <textarea name="content" id="content" v-model="baseData.content" required></textarea>
+            <div class="my-5 input-row">
+                <div class="input-container">
+                    <!-- contenuto  -->
+                    <label for="section">Contenuto:</label>
+                    <div>
+                        <div class="relative">
+
+                            <textarea @input="updateCharacters('section')" class="bigger-textarea pb-4 pt-1" rows="4"
+                                name="section" id="section" v-model="baseData.section"></textarea>
+                            <p v-if="sectionCharCount > 0" class="absolute right-[8px] bottom-[8px]">
+                                <span
+                                    :class="[sectionCharCount <= minChar || sectionCharCount >= maxSectionChar ? 'text-[#f60001]' : '']">
+                                    {{sectionCharCount}}</span> /
+                                <span
+                                    :class="[sectionCharCount <= minChar || sectionCharCount >= maxSectionChar ? 'text-[#f60001]' : '']">{{
+                                    maxSectionChar }}</span>
+                            </p>
+                        </div>
+                        <div class="errors-container">
+                            <p v-for="error in sectionErrors">{{ error }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <!-- pubblicato  -->
+                    <label>Pubblicato:</label>
+                    <div>
+                        <label for="published-true">Si</label>
+                        <input checked type="radio" id="published-true" name="published" value="true"
+                            v-model="baseData.published">
+                    </div>
+                    <div>
+                        <label for="published-false">No</label>
+                        <input type="radio" id="published-false" name="published" value="false"
+                            v-model="baseData.published">
+                    </div>
+                    <div class="errors-container">
+                        <p v-for="error in publishedErrors">{{ error }}</p>
+                    </div>
+                </div>
             </div>
-            <div class="flex items-center justify-center mt-10">
+            <div class="my-5 input-row">
+                <!-- categoria  -->
+                <label>Scegli una categoria:</label>
+            </div>
+            <div class="my-5 input-row" v-if="categories.length > 0">
+                <div v-for="category in categories" :key="category.id">
+                    <label :for="category.name">{{ category.name }}</label>
+                    <input type="radio" :id="category.name" :name="category.id" :value="category.id"
+                        v-model="baseData.categoryId">
+                </div>
+            </div>
+            <div class="my-5 input-row">
+                <div class="errors-container">
+                    <p v-for="error in categoryErrors">{{ error }}</p>
+                </div>
+            </div> 
+            <!-- button  -->
+            <div class="flex items-center justify-center mt-10 my-4">
                 <RouterLink class=" custom-span" to="/">
                     Torna indietro
                 </RouterLink>
-                <button class="custom-span ml-5">Crea</button>
+                <button class="custom-span ml-5">Crea <i class="fa-solid fa-plus"></i></button>
             </div>
         </form>
 
@@ -61,36 +241,55 @@ const sendPost = async () => {
 </template>
 
 <style  scoped>
+
+
 main {
     color: var(--custom-black);
-    border: 2px solid var(--custom-purple);
-    background-color: white;
-    width: 500px;
+    margin: 2rem auto;
+    
+}
+.input-row {
+    display: flex;
+    align-items: start;
+    justify-content: space-evenly;
+    gap: 30px;
+    flex-wrap: wrap;
     padding: 1rem;
+}
+
+.input-container {
+    display: flex;
+    align-items: start;
+    gap: 10px;
+}
+
+.form-container {
+    border: 2px solid var(--custom-purple);
+    background-color: transparent;
+    width: 80%;
+    /* padding: 1rem; */
     border-radius: 10px;
+    margin: 2rem auto;
 }
-
-/* input, textarea {
-border: 2px solid var(--custom-blue);
-border-radius: 10px;
-padding-left: 10px;
-
-}
-
-input:focus-visible,
-textarea:focus-visible {
-    border-color: var(--custom-indaco);
-    outline: 0;
-} */
 
 label {
     margin-right: 5px;
 }
 
-.input-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 30px;
+
+input[type='text'],
+textarea {
+    width: 300px;
+}
+
+
+
+.bigger-textarea {
+    width: 400px;
+    height: 200px;
+}
+
+.errors-container {
+    color: var(--custom-red);
 }
 </style>
