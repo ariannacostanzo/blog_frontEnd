4228d5b7a409256355986f92f140c97d29f24251
ariannacostanzo <costanzo.arianna@outlook.it>
2024-10-01 19:53:52 +0200

    created custom loader

--- src/components/LoaderApi.vue
+++ src/components/LoaderApi.vue
@@ -1,16 +1,43 @@
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+const isLoading = ref(true);
+const loadingProgress = ref(0);
+let intervalId = null;
+
+const startLoading = () => {
+  intervalId = setInterval(() => {
+    if (loadingProgress.value < 100) {
+      loadingProgress.value += 1; 
+    } else {
+      clearInterval(intervalId); 
+    }
+  }, 1); 
+};
+
+onMounted(() => {
+  startLoading(); 
+});
+
+onBeforeUnmount(() => {
+  clearInterval(intervalId);
+});
 </script>
 
 <template>
-  <div>
+  <div class="loader-container">
 
-    <!-- <i class="fa-solid fa-spinner fa-spin text-5xl"></i> -->
-    <img src="../assets/images/loading.gif" alt="loading">
+    <div class="my-14">
+      <h2>Loading</h2>
+      <div class="loader">
+        <div class="loading" :style="{ width: `${loadingProgress}%` }"></div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <style scoped>
-  div {
+  .loader-container {
     position: absolute;
     left: 0;
     right: 0;
@@ -21,8 +48,30 @@
     margin-top: 10rem;
   }
 
+  h2 {
+    font-size: 28px;
+    color: var(--custom-white);
+    text-shadow: 1px 1px 2px black;
+    text-align: center;
+  }
+
+  .loader {
+    border: 3px solid var(--custom-green);
+    height: 35px;
+    padding: 5px;
+    width: 350px;
+  }
+
+  .loading {
+    background-color: var(--custom-green);
+    /* width: 100%; */
+    height: 100%;
+  }
+  
   img {
     width: 500px;
     height: 200px;
   }
 </style>
+
+<!-- fare un vero loader  -->
